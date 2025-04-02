3a538b608a1f83300f731680b0473c8dcbc18552
Alejo <alejodfr@gmail.com>
2025-04-02 18:43:29 -0500

    Type inference a null variable declaration

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,13 @@
 
-val name: String = "Nate"
-var greeting: String = "hello"
+//Just explaining how type inference and null variables works in kotlin
+val name = "Nate"
+var greeting: String? = null
 
 fun main(){
     println(greeting)
     println(name)
 
+    greeting = null
     greeting = "hi"
     println(greeting)
-    println(name)
 }
