586cdec74cd294a235f0fef40260b8d7e45eb0ff
Alejo <alejodfr@gmail.com>
2025-04-03 09:04:56 -0500

    Colleciones e iteracciones parte 9 mas cambios

--- src/Main.kt
+++ src/Main.kt
@@ -10,7 +10,7 @@ fun sayHello(greeting: String, vararg itemToGreet:String) {
 fun greetPerson(greeting: String = "hello", name: String = "kotlin") = println("$greeting $name")
 
 fun main(){
-    greetPerson()
-    greetPerson(name = "alejo")
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    sayHello(itemToGreet = *interestingThings, greeting = "hi")
 }
 
