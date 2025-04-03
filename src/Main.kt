4febb8c7de34ee22ccb688eeb541648446ae3bf9
Alejo <alejodfr@gmail.com>
2025-04-02 20:40:51 -0500

    Colleciones e iteracciones parte 2.1 (usando metodos de una libreria externa en este caso el forEach, otra forma de hacerlo)

--- src/Main.kt
+++ src/Main.kt
@@ -10,8 +10,8 @@ fun main(){
     println(interestingThings[0]) //imprime el primer elemento del array porque los arreglos comienzas desde el zero
     println(interestingThings.get(0)) //hace lo mismo que la linea superior
 
-   interestingThings.forEach {
-        println(it) //it is the default name for each element in the array
+   interestingThings.forEach { interestingThings ->
+        println(interestingThings) //otrs forma de hacerlo
    }
 }
 
