47c9e70378f0d6259f47c8247c43ece546d9eda1
Alejo <alejodfr@gmail.com>
2025-04-02 20:04:43 -0500

    Funciones basicas Parte 2 (uso de Unit)

--- src/Main.kt
+++ src/Main.kt
@@ -5,8 +5,13 @@ fun getGreeting(): String {
     return "Hello Kotlin"
 }
 
+fun sayHello(): Unit{
+    println(getGreeting())
+}
+
 fun main(){
     println("hello world")
     println(getGreeting())
+    sayHello()
 }
 
