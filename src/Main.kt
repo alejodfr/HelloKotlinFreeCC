2a3efefcc5bf1d5e8ed32a36333b77a840790c3f
Alejo <alejodfr@gmail.com>
2025-04-02 19:49:49 -0500

    another variable control flow example 2.1

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,10 @@
 
-//mas ejemplos de control de variables 2
+//mas ejemplos de control de variables 2.1
 val name = "Nate"
 var greeting: String? = null
 
 fun main(){
+    greeting = "hello"
     val greetingToPrint = when (greeting) {
         null -> "hi"
         else -> greeting
