627e7c745cabc5ae52260155319f29f2e13cece0
Alejo <alejodfr@gmail.com>
2025-04-02 19:57:31 -0500

    Funciones basicas Parte 1 (creacion de una funcion que devuelve un String)

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,12 @@
 
-//mas ejemplos de control de variables 2.1
-val name = "Nate"
-var greeting: String? = null
+// explicacion de funciones basicas
+
+fun getGreeting(): String {
+    return "Hello Kotlin"
+}
 
 fun main(){
-    greeting = "hello"
-    val greetingToPrint = when (greeting) {
-        null -> "hi"
-        else -> greeting
-    }
-    println(greetingToPrint)
-    println(name)
-    println(greeting)
+    println("hello world")
+    println(getGreeting())
 }
+
